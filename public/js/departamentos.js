ea833de76623190d0e58c2f5688d22cd1d57aec0
Alejandro Landeros Contreras <alanderos>
2015-06-02 20:30:13 -0500

    Merge branch 'master'

--- public/js/departamentos.js
+++ public/js/departamentos.js
@@ -12,3 +12,63 @@ $(document).ready(function(){
         $(this).addClass("botonClick");
     });
 });
+
+function botonesClick(){
+    $("#botonDepto1").click(function(){
+        console.log("click en boton 1");
+        $(".deptos").hide();
+        $("#depto1").show();
+    });
+    $("#botonDepto2").click(function(){
+        $(".deptos").hide();
+        $("#depto2").show();
+    });
+    $("#botonDepto3").click(function(){
+        $(".deptos").hide();
+        $("#depto3").show();
+    });
+    $("#botonDepto4").click(function(){
+        $(".deptos").hide();
+        $("#depto4").show();
+    });
+    $("#botonDepto5").click(function(){
+        $(".deptos").hide();
+        $("#depto5").show();
+    });
+    $("#botonDepto6").click(function(){
+        $(".deptos").hide();
+        $("#depto6").show();
+    });
+    $("#botonDepto7").click(function(){
+        $(".deptos").hide();
+        $("#depto7").show();
+    });
+    $("#botonDepto8").click(function(){
+        $(".deptos").hide();
+        $("#depto8").show();
+    });
+    $("#botonDepto9").click(function(){
+        $(".deptos").hide();
+        $("#depto9").show();
+    });
+    $("#botonDepto10").click(function(){
+        $(".deptos").hide();
+        $("#depto10").show();
+    });
+    $("#botonDepto11").click(function(){
+        $(".deptos").hide();
+        $("#depto11").show();
+    });
+    $("#botonDepto12").click(function(){
+        $(".deptos").hide();
+        $("#depto12").show();
+    });
+    $("#botonDepto13").click(function(){
+        $(".deptos").hide();
+        $("#depto13").show();
+    });
+    $("#botonDepto14").click(function(){
+        $(".deptos").hide();
+        $("#depto14").show();
+    });
+}
