6f2cf31151a46c0d073cc830869af0e71396312d
Alejandro Landeros Contreras <alanderos>
2015-06-03 15:47:27 -0500

    mapa de la tienda e informacion de la tienda

--- public/js/departamentos.js
+++ public/js/departamentos.js
@@ -15,60 +15,62 @@ $(document).ready(function(){
 
 function botonesClick(){
     $("#botonDepto1").click(function(){
-        console.log("click en boton 1");
-        $(".deptos").hide();
-        $("#depto1").show();
-    });
-    $("#botonDepto2").click(function(){
-        $(".deptos").hide();
-        $("#depto2").show();
-    });
-    $("#botonDepto3").click(function(){
-        $(".deptos").hide();
-        $("#depto3").show();
-    });
-    $("#botonDepto4").click(function(){
-        $(".deptos").hide();
-        $("#depto4").show();
-    });
-    $("#botonDepto5").click(function(){
-        $(".deptos").hide();
-        $("#depto5").show();
-    });
-    $("#botonDepto6").click(function(){
-        $(".deptos").hide();
-        $("#depto6").show();
-    });
-    $("#botonDepto7").click(function(){
+        
         $(".deptos").hide();
         $("#depto7").show();
     });
-    $("#botonDepto8").click(function(){
+    $("#botonDepto2").click(function(){
+        $(".deptos").hide();
+        $("#depto3").show();
+    });
+    $("#botonDepto3").click(function(){
         $(".deptos").hide();
         $("#depto8").show();
     });
-    $("#botonDepto9").click(function(){
-        $(".deptos").hide();
-        $("#depto9").show();
-    });
-    $("#botonDepto10").click(function(){
-        $(".deptos").hide();
-        $("#depto10").show();
-    });
-    $("#botonDepto11").click(function(){
+    $("#botonDepto4").click(function(){
         $(".deptos").hide();
         $("#depto11").show();
     });
-    $("#botonDepto12").click(function(){
+    $("#botonDepto5").click(function(){
+        $(".deptos").hide();
+        
+        $("#depto14").show();
+    });
+    $("#botonDepto6").click(function(){
+        $(".deptos").hide();
+        $("#depto15").show();
+    });
+    $("#botonDepto7").click(function(){
+        $(".deptos").hide();
+        $("#depto9").show();
+    });
+    $("#botonDepto8").click(function(){
         $(".deptos").hide();
         $("#depto12").show();
+        $("#depto13").show();
+    });
+    $("#botonDepto9").click(function(){
+        $(".deptos").hide();
+        $("#depto2").show();
+    });
+    $("#botonDepto10").click(function(){
+        $(".deptos").hide();
+        $("#depto6").show();
+    });
+    $("#botonDepto11").click(function(){
+        $(".deptos").hide();
+        $("#depto5").show();
+    });
+    $("#botonDepto12").click(function(){
+        $(".deptos").hide();
+        $("#depto1").show();
     });
     $("#botonDepto13").click(function(){
         $(".deptos").hide();
-        $("#depto13").show();
+        $("#depto4").show();
     });
     $("#botonDepto14").click(function(){
         $(".deptos").hide();
-        $("#depto14").show();
+        $("#depto10").show();
     });
 }
